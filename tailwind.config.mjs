e42bb828c1faf276280c23a21f1c0054e6b63de3
IIRDCodeII <rdcode.dev@gmail.com>
2024-12-26 21:37:18 -0500

    refac(style): Setup system desing color (woodsmoke & carnation) on blog project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'selector',
-	theme: {
-		extend: {
-			colors: {
-				background: "rgb(var(--background))"
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'selector',
+  theme: {
+    colors: {
+      'woodsmoke': {
+        '50': '#f7f7f8',
+        '100': '#ededf1',
+        '200': '#d7d8e0',
+        '300': '#b5b6c4',
+        '400': '#8c8ea4',
+        '500': '#6e7189',
+        '600': '#595a70',
+        '700': '#48495c',
+        '800': '#3e3e4e',
+        '900': '#373843',
+        '950': '#0e0e11',
+      },
+      'carnation': {
+        '50': '#fef2f2',
+        '100': '#fee2e2',
+        '200': '#fdcbcb',
+        '300': '#fba7a6',
+        '400': '#f67170',
+        '500': '#ed4746',
+        '600': '#da2928',
+        '700': '#b71f1e',
+        '800': '#971e1d',
+        '900': '#7e1f1e',
+        '950': '#440b0b',
+
+      }
+    },
+    fontFamily: {
+      'poppins': ['']
+    }
+  },
+  plugins: [],
 }
